513ade6b7b96657d4f941d43e3ac88e254590c7d
martin <sf@sgv.com>
2020-08-21 10:32:28 +0200

    oppdatert kode

---
+++ question_1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+customers = ['James', 'John', 'Robert', 'Mary', 'Patricia', 'Jennifer']
+salary = [155000, 755000,  455000, 1255000, 635000, 575000]
+taxes = [55800, 317100, 182000, 451800, 171450, 71400]
+
+perc = [(i / j)*100 for i, j in zip(taxes, salary)]
+print(salary)
+print(perc)
